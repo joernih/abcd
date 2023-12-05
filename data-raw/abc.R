d6511ca43405bc38dc2a6e451c1d50d02b1105fe
joernih <jorn.halvorsen@gmail.com>
2023-12-05 16:09:01 +0100

    somch

---
+++ data-raw/abc.R
@@ -0,0 +1,24 @@
+biler <- read.csv("biler.csv")
+usethis::use_data(biler, overwrite = TRUE)
+head(biler)
+
+abc.R
+
+#abc_out.data
+#abc_out.js.metadata
+#biler.csv
+#biler.gdt
+#boliger.csv
+#boliger.gdt
+#elbiler.csv
+#elbiler.gdt
+#feriereiser.gdt
+#luftkvalitet.csv
+#luftkvalitet.gdt
+#pizza.csv
+#pizza.gdt
+#tidsserier.gdt
+#toyota.csv
+#toyota.gdt
+#vareAB.csv
+#vareAB.gdt
--- data-raw/csv/abc.R
+++
@@ -1,3 +0,0 @@
-biler <- read.csv("biler.csv")
-usethis::use_data(biler, overwrite = TRUE)
-head(biler)
