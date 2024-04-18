a978408f3c3e64d84685f346676a3b6be9a25ca7
joernih <jorn.halvorsen@gmail.com>
2024-04-18 22:24:17 +0200

    abc

--- data-raw/datawasmcsv.R
+++ data-raw/datawasmcsv.R
@@ -1,3 +1,52 @@
+library(ExchangeRate)
+library(Hyperinflation)
+library(ggplot2)
+library(purrr)
+library(fredr)
+library(DataEditR)
+library(gretlR)
+################################################################################################################
+## Norges Bank ##
+################################################################################################################
+nbjson <- function(fre="B",dint=c("2016-10-17","2024-01-01"),vexr=c("USD","EUR","SEK")){
+  frekh <- list("A"="year","M"="month","B"="day")[[fre]]
+  url <- paste0(
+		"https://data.norges-bank.no/api/data/EXR/"
+		,fre,
+		".",
+                paste(vexr, collapse = '+'),
+		".NOK.SP?format=sdmx-json&startPeriod="
+		,dint[1],
+		"&endPeriod=",
+		dint[2],
+		"&locale=no"
+  )
+  gjd <- httr::GET(url)
+  con <- httr::content(gjd, as = "text")
+  jda <- jsonlite::fromJSON(con)
+  lex <- sapply(c(1:length(vexr)),function(x){
+        t(as.numeric(as.matrix(jda$data$dataSets$series[[x]]$observations)))
+  })
+  nlexl <- as.data.frame(lex) %>% data.table::setnames(new=vexr) %>%
+	  dplyr::mutate(dato=seq.Date(as.Date(dint[1]),by=frekh,length.out=dim(lex)[1])) %>%
+          dplyr::select(dato, everything()) %>%
+          tidyr::pivot_longer(
+            cols = -dato,
+            names_to = "rate",
+            values_to = "value") %>%
+	  dplyr::arrange(rate,dato) %>% dplyr::group_by(rate) %>%
+          dplyr::mutate(raten = value/value[1]) %>%
+          dplyr::mutate(percn = raten/raten[1]) %>%
+          dplyr::ungroup()
+
+  nlexw <- tidyr::pivot_wider(nlexl,names_from=rate,values_from=c(value,raten,percn))
+  return(list(nlexw,nlexl,con,jda))
+}
+nb_ts <- lapply(c("A","M","B"),function(x){
+  nbjson(fre=x,dint=c("2016-10-17","2024-01-01"),vexr=c("USD","EUR","SEK"))
+})
+usethis::use_data(nb_ts, overwrite = TRUE)
+###############################################################################################################
 usedvw <- gretlReadWrite::read.gdt("gdt/usedvw.gdt")
 newcars <- gretlReadWrite::read.gdt("gdt/newcars.gdt")
 #icecream <- gretlReadWrite::read.gdt("ex1_linear/icecream.gdt")
@@ -12,27 +61,10 @@ usethis::use_data(icecream,overwrite=TRUE)
 usethis::use_data(adverticing,overwrite=TRUE)
 usethis::use_data(tssales,overwrite=TRUE)
 ###############################################################################################################
-library(Hyperinflation)
-library(ggplot2)
-library(purrr)
-library(fredr)
-library(DataEditR)
-library(gretlR)
 usedvw <- gretlReadWrite::read.gdt("gdt/usedvw.gdt")
 newcars <- gretlReadWrite::read.gdt("gdt/newcars.gdt")
 usethis::use_data(usedvw, overwrite = TRUE)
 usethis::use_data(newcars, overwrite = TRUE)
-################################################################################################################
-## Book ##
-################################################################################################################
-csvabc <- c("abc_out.data", "biler.gdt", "elbiler.csv", "luftkvalitet.csv", "pizza.csv", "toyota.csv", "vareAB.gdt", "abc_out.
-js.metadata", "boliger.csv", "elbiler.gdt", "luftkvalitet.gdt", "pizza.gdt", "toyota.gdt", "biler.csv", "boliger.gdt
-", "feriereiser.gdt", "luftkvalitet.gdt~", "tidsserier.gdt", "vareAB.csv")
-
-abc <- read.csv(paste0("csv/",csvabc[4]))
-usethis::use_data(abc, overwrite = TRUE)
-# usethis::use_data(bjki_ts, overwrite = TRUE)
-open_gdt(fpath, mkstruct = TRUE, info = TRUE)
 ################################################################################################################
 ## Fred ##
 ################################################################################################################
@@ -73,41 +105,14 @@ usethis::use_data(fred_ts, overwrite = TRUE)
 #=c(USA=c('MABMM301USM189S'),JPN=c('MABMM301JPM189S'),EUZ=c('MABMM301EZM189S'),GBR=c('MABMM301GBM189S'),CAN=c('MABMM301CAM189S'),NOR=c('MABMM301NOM189S'),DEN='MABMM301DKM189N',SWE='MABMM301SEM189N',SKE='MABMM301KRM189S'),
 #MABMM301USM189S,MABMM301EZM189S,MABMM301NOM189S
 ################################################################################################################
-## Norges Bank ##
-nbjson <- function(fre="B",dint=c("2016-10-17","2024-01-01"),vexr=c("USD","EUR","SEK")){
-  frekh <- list("A"="year","M"="month","B"="day")[[fre]]
-  url <- paste0(
-		"https://data.norges-bank.no/api/data/EXR/"
-		,fre,
-		".",
-                paste(vexr, collapse = '+'),
-		".NOK.SP?format=sdmx-json&startPeriod="
-		,dint[1],
-		"&endPeriod=",
-		dint[2],
-		"&locale=no"
-  )
-  gjd <- httr::GET(url)
-  con <- httr::content(gjd, as = "text")
-  jda <- jsonlite::fromJSON(con)
-  lex <- sapply(c(1:length(vexr)),function(x){
-        t(as.numeric(as.matrix(jda$data$dataSets$series[[x]]$observations)))
-  })
-  nlexl <- as.data.frame(lex) %>% data.table::setnames(new=vexr) %>%
-	  dplyr::mutate(dato=seq.Date(as.Date(dint[1]),by=frekh,length.out=dim(lex)[1])) %>%
-          dplyr::select(dato, everything()) %>%
-          tidyr::pivot_longer(
-            cols = -dato,
-            names_to = "rate",
-            values_to = "value") %>%
-	  dplyr::arrange(rate,dato) %>% dplyr::group_by(rate) %>%
-          dplyr::mutate(raten = value/value[1]) %>%
-          dplyr::mutate(percn = raten/raten[1]) %>%
-          dplyr::ungroup()
+################################################################################################################
+## Book ##
+################################################################################################################
+csvabc <- c("abc_out.data", "biler.gdt", "elbiler.csv", "luftkvalitet.csv", "pizza.csv", "toyota.csv", "vareAB.gdt", "abc_out.
+js.metadata", "boliger.csv", "elbiler.gdt", "luftkvalitet.gdt", "pizza.gdt", "toyota.gdt", "biler.csv", "boliger.gdt
+", "feriereiser.gdt", "luftkvalitet.gdt~", "tidsserier.gdt", "vareAB.csv")
 
-  nlexw <- tidyr::pivot_wider(nlexl,names_from=rate,values_from=c(value,raten,percn))
-  return(list(nlexw,nlexl))
-}
-nb_ts <- list(kurs=nbjson())
-usethis::use_data(nb_ts, overwrite = TRUE)
-
+abc <- read.csv(paste0("csv/",csvabc[4]))
+usethis::use_data(abc, overwrite = TRUE)
+# usethis::use_data(bjki_ts, overwrite = TRUE)
+open_gdt(fpath, mkstruct = TRUE, info = TRUE)
